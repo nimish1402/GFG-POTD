c001c061e7805b297125ee45e499db34f3fd4549
Nimish Selot <121348792+nimish1402@users.noreply.github.com>
2024-09-10 23:42:27 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Basic/Java Switch Case statement/java-switch-case-statement.cpp
@@ -0,0 +1,83 @@
+//{ Driver Code Starts
+#include<bits/stdc++.h>
+using namespace std;
+
+
+class Array
+{
+public:
+    template <class T>
+    static void input(vector<T> &A,int n)
+    {
+        for (int i = 0; i < n; i++)
+        {
+            scanf("%lf ",&A[i]);
+        }
+    }
+
+    template <class T>
+    static void print(vector<T> &A)
+    {
+        cout<<setprecision(3)<<fixed;
+        for (int i = 0; i < A.size(); i++)
+        {
+            cout << A[i]<< " ";
+        }
+        cout << endl;
+    }
+};
+
+
+// } Driver Code Ends
+
+class Solution {
+  public:
+    double switchCase(int choice, vector<double> &arr) {
+        
+        if(choice == 1){
+            int R = arr[0];
+            return M_PI * R*R;
+        } 
+        else if(choice == 2){
+            int L = arr[0];
+            int B = arr[1];
+            return L*B;
+        }
+        else{
+            return -1;
+        }
+    }
+};
+
+
+//{ Driver Code Starts.
+
+int main(){
+    int t;
+    scanf("%d ",&t);
+    while(t--){
+        
+        int choice;
+        scanf("%d",&choice);
+        
+        
+        vector<double> arr(choice);
+        Array::input(arr,choice);
+        
+        Solution obj;
+        double res = obj.switchCase(choice, arr);
+        // cout<<fixed<<endl;
+        if(res == int(res)){
+            cout<<int(res)<<endl;
+            continue;
+        }
+        cout<<setprecision(2)<<fixed;
+        // 
+        // }else{
+            cout<<res<<endl;
+        // }
+        
+    }
+}
+
+// } Driver Code Ends
