e5e9ea1eefe67bb10b8433f28d3ec3cc3e86157f
Nimish Selot <121348792+nimish1402@users.noreply.github.com>
2024-11-25 10:20:11 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Count Inversions/count-inversions.cpp
@@ -0,0 +1,83 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+class Solution {
+  public:
+   
+    long long Count = 0;
+
+    void merge(vector<int>& arr, int low, int mid, int high) {
+        vector<int> temp;
+        int left = low;
+        int right = mid + 1;
+
+        while (left <= mid && right <= high) {
+            if (arr[left] <= arr[right]) {
+                temp.push_back(arr[left]);
+                left++;
+            } else {
+                // Count inversions
+                Count += (mid - left + 1);
+                temp.push_back(arr[right]);
+                right++;
+            }
+        }
+
+        while (left <= mid) {
+            temp.push_back(arr[left]);
+            left++;
+        }
+
+        while (right <= high) {
+            temp.push_back(arr[right]);
+            right++;
+        }
+
+        for (int i = low; i <= high; i++) {
+            arr[i] = temp[i - low];
+        }
+    }
+
+    void mergeSort(vector<int>& arr, int low, int high) {
+        if (low >= high) return;
+        int mid = (low + high) / 2;
+        mergeSort(arr, low, mid);
+        mergeSort(arr, mid + 1, high);
+        merge(arr, low, mid, high);
+    }
+
+    
+    int inversionCount(vector<int> &arr) {
+        mergeSort(arr, 0, arr.size() - 1);
+        return Count;
+    }
+};
+
+//{ Driver Code Starts.
+
+int main() {
+
+    int T;
+    cin >> T;
+    cin.ignore();
+    while (T--) {
+        int n;
+        vector<int> a;
+        string input;
+        getline(cin, input);
+        stringstream ss(input);
+        int num;
+        while (ss >> num)
+            a.push_back(num);
+        Solution obj;
+        cout << obj.inversionCount(a) << endl;
+        cout << "~" << endl;
+    }
+
+    return 0;
+}
+
+// } Driver Code Ends
