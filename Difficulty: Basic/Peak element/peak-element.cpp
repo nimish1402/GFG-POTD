67b1e3203fef84f2eeeb5e91c05c8008d112b21f
Nimish Selot <121348792+nimish1402@users.noreply.github.com>
2024-12-15 19:27:02 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Basic/Peak element/peak-element.cpp
@@ -0,0 +1,74 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+
+class Solution {
+  public:
+    int peakElement(vector<int> &arr) {
+        int left = 0;
+        int right = arr.size() - 1;
+
+        while (left < right) {
+            int mid = left + (right - left) / 2;
+
+            // If mid element is less than the next element, move to the right half
+            if (arr[mid] < arr[mid + 1]) {
+                left = mid + 1;
+            } else {
+                // Else, move to the left half (including mid)
+                right = mid;
+            }
+        }
+
+        // Left and right converge to the peak index
+        return left;
+    }
+};
+
+//{ Driver Code Starts.
+
+int main() {
+    int t;
+    cin >> t;
+    cin.ignore();
+    while (t--) {
+        vector<int> a;
+        string input;
+        getline(cin, input);
+        stringstream ss(input);
+        int number;
+        while (ss >> number) {
+            a.push_back(number);
+        }
+
+        Solution ob;
+        int idx = ob.peakElement(a);
+        int n = a.size();
+        bool f = 0;
+        if (idx < 0 and idx >= n)
+            cout << "false" << endl;
+        else {
+            if (n == 1 and idx == 0)
+                f = 1;
+            else if (idx == 0 and a[0] > a[1])
+                f = 1;
+            else if (idx == n - 1 and a[n - 1] > a[n - 2])
+                f = 1;
+            else if (a[idx] > a[idx + 1] and a[idx] > a[idx - 1])
+                f = 1;
+            else
+                f = 0;
+            if (f)
+                cout << "true" << endl;
+            else
+                cout << "false" << endl;
+        }
+        cout << "~" << endl;
+    }
+    return 0;
+}
+
+// } Driver Code Ends
