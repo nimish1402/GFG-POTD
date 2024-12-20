e72ff025e4aeb38bfdf492c30bf55c4a0173f42d
Nimish Selot <121348792+nimish1402@users.noreply.github.com>
2024-12-20 18:04:35 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Spirally traversing a matrix/spirally-traversing-a-matrix.cpp
@@ -0,0 +1,78 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+class Solution {
+  public:
+    vector<int> spirallyTraverse(vector<vector<int> > &mat) {
+        vector<int> result;
+        if (mat.empty()) return result;
+
+        int top = 0, bottom = mat.size() - 1;
+        int left = 0, right = mat[0].size() - 1;
+
+        while (top <= bottom && left <= right) {
+            // Traverse from left to right on the top boundary
+            for (int i = left; i <= right; ++i) {
+                result.push_back(mat[top][i]);
+            }
+            ++top;
+
+            // Traverse from top to bottom on the right boundary
+            for (int i = top; i <= bottom; ++i) {
+                result.push_back(mat[i][right]);
+            }
+            --right;
+
+            // Traverse from right to left on the bottom boundary (if not already visited)
+            if (top <= bottom) {
+                for (int i = right; i >= left; --i) {
+                    result.push_back(mat[bottom][i]);
+                }
+                --bottom;
+            }
+
+            // Traverse from bottom to top on the left boundary (if not already visited)
+            if (left <= right) {
+                for (int i = bottom; i >= top; --i) {
+                    result.push_back(mat[i][left]);
+                }
+                ++left;
+            }
+        }
+
+        return result;
+    }
+};
+
+//{ Driver Code Starts.
+int main() {
+    int t;
+    cin >> t;
+
+    while (t--) {
+        int r, c;
+        cin >> r >> c;
+        vector<vector<int>> matrix(r);
+
+        for (int i = 0; i < r; i++) {
+            matrix[i].assign(c, 0);
+            for (int j = 0; j < c; j++) {
+                cin >> matrix[i][j];
+            }
+        }
+
+        Solution ob;
+        vector<int> result = ob.spirallyTraverse(matrix);
+        for (int i = 0; i < result.size(); ++i)
+            cout << result[i] << " ";
+        cout << endl;
+
+        cout << "~"
+             << "\n";
+    }
+    return 0;
+}
+// } Driver Code Ends
