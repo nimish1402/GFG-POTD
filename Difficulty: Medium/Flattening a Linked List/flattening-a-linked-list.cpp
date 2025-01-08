252222cb438724d0705ec3733c04ea7191cd8ec1
Nimish Selot <121348792+nimish1402@users.noreply.github.com>
2025-01-08 20:58:23 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Flattening a Linked List/flattening-a-linked-list.cpp
@@ -0,0 +1,132 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+struct Node {
+    int data;
+    struct Node *next;
+    struct Node *bottom;
+
+    Node(int x) {
+        data = x;
+        next = NULL;
+        bottom = NULL;
+    }
+};
+
+void printList(Node *Node) {
+    while (Node != NULL) {
+        printf("%d ", Node->data);
+        Node = Node->bottom;
+    }
+    printf("\n");
+}
+
+
+// } Driver Code Ends
+/* Node structure  used in the program
+
+struct Node{
+    int data;
+    struct Node * next;
+    struct Node * bottom;
+
+    Node(int x){
+        data = x;
+        next = NULL;
+        bottom = NULL;
+    }
+
+};
+*/
+
+class Solution {
+  public:
+    Node* convert(const vector<int>& arr){
+        if(arr.size() == 0){
+            return NULL;
+        }
+        Node* head = new Node(arr[0]);
+        Node* temp = head;
+        
+        for(int i =1; i <arr.size(); i++){
+            Node* newNode = new Node(arr[i]);
+            temp->bottom = newNode;
+            temp = temp->bottom;
+        }
+        return head;
+    }
+    Node *flatten(Node *head) {
+        vector<int> arr ={};
+        Node* temp = head;
+        
+        while(temp != NULL){
+            Node* t2 = temp;
+            while(t2!= NULL){
+                arr.push_back(t2->data);
+                t2 = t2->bottom;
+            }
+            temp = temp->next;
+        }
+        
+        sort(arr.begin(), arr.end());
+        
+        head = convert(arr);
+        
+        return head;
+    }
+};
+
+
+//{ Driver Code Starts.
+
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int n;
+        vector<int> work;
+        string input;
+        getline(cin, input); // Read the entire line for the array elements
+        getline(cin, input); // Read the entire line for the array elements
+        stringstream ss(input);
+        int number;
+        while (ss >> number) {
+            work.push_back(number);
+        }
+        n = work.size();
+
+        Node *head = NULL;
+        Node *pre = NULL;
+
+        for (int i = 0; i < n; i++) {
+            int m = work[i] - 1;
+            int data;
+            cin >> data;
+            Node *temp = new Node(data);
+            if (head == NULL) {
+                head = temp;
+                pre = temp;
+            } else {
+                pre->next = temp;
+                pre = temp;
+            }
+
+            Node *preB = temp;
+            for (int j = 0; j < m; j++) {
+                int temp_data;
+                cin >> temp_data;
+                Node *tempB = new Node(temp_data);
+                preB->bottom = tempB;
+                preB = tempB;
+            }
+        }
+
+        Solution ob;
+        Node *root = ob.flatten(head);
+        printList(root);
+    }
+    return 0;
+}
+
+// } Driver Code Ends
