cfd7f7410608dc969aeb7e61bdb65ac3cced2e2b
Nimish Selot <121348792+nimish1402@users.noreply.github.com>
2025-06-20 12:31:10 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Topological sort/topological-sort.cpp
+++ Difficulty: Medium/Topological sort/topological-sort.cpp
@@ -1,38 +1,44 @@
 class Solution {
   private:
     void dfs(int node, vector<int>& vis, stack<int>& st, const vector<vector<int>>& adj){
-        vis[node] = 1;
-        for(auto it : adj[node]){
-            if(!vis[it]){
-                dfs(it, vis, st, adj);
-            }
-        }
-        st.push(node);
+        
     }
   public:
     vector<int> topoSort(int V, vector<vector<int>>& edges) {
         vector<vector<int>> adj(V);
-        
         for(const auto& edge : edges){
             int u = edge[0];
             int v = edge[1];
             adj[u].push_back(v);
         }
         
-        vector<int> vis(V, 0);
-        stack<int> st;
-        
+        // indegree array 
+        vector<int> indegree(V, 0);
         for(int i = 0; i < V; i++){
-            if(!vis[i]){
-                dfs(i, vis, st, adj);
+            for(auto it: adj[i]){
+                indegree[it]++;
             }
         }
         
-        vector<int> ans;
-        while(!st.empty()){
-            ans.push_back(st.top());
-            st.pop();
+        queue<int> q;
+        
+        for(int i = 0; i < V; i++){
+            if(indegree[i] == 0){
+                q.push(i);        
+            }
         }
-        return ans;
+        vector<int> topo;
+        while(!q.empty()){
+            int node = q.front();
+            q.pop();
+            topo.push_back(node);
+            
+            for(auto it: adj[node]){
+                indegree[it]--;
+                if(indegree[it] == 0) q.push(it);
+            }
+        }
+        
+        return topo;
     }
 };
