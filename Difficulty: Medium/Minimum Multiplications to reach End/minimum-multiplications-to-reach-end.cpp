33171d3bf3618f2d4114a2ee258abb0564b5050a
Nimish Selot <121348792+nimish1402@users.noreply.github.com>
2025-06-26 19:15:09 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Minimum Multiplications to reach End/minimum-multiplications-to-reach-end.cpp
@@ -0,0 +1,31 @@
+// User function Template for C++
+
+class Solution {
+  public:
+    int minimumMultiplications(vector<int>& arr, int start, int end) {
+        
+        queue<pair<int, int>> q;
+        q.push({start, 0});
+        vector<int> dist(100000, 1e9);
+        dist[start] = 0;
+        int mod = 100000;
+        
+        if(start == end ) return 0;
+        
+        while(!q.empty()){
+            int node = q.front().first;
+            int steps = q.front().second;
+            q.pop();
+            
+            for(auto it : arr){
+                int num = (it * node) % mod;
+                if(steps+ 1 < dist[num]){
+                    dist[num] = steps + 1;
+                    if(num == end) return steps + 1;
+                    q.push({num, steps+1});
+                }
+            }
+        }
+        return -1;
+    }
+};
